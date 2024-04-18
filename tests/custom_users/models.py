5baa522824b7f8cea4ae92ecd06e626abc0d99b9
Dennis Bunskoek <dbunskoek@leukeleu.nl>
2024-04-18 18:58:05 +0200

    Implement `list_staff_users` based on `list_superusers`

--- tests/custom_users/models.py
+++ tests/custom_users/models.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 
 
-class NoSuperUser(AbstractBaseUser):
+class NoSuperuser(AbstractBaseUser):
+    pass
+
+
+class NoStaff(AbstractBaseUser):
     pass
